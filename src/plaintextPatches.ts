95a5030226cbd2135f335460a52e9f5f7011163d
Tharki-god <alokender018@gmail.com>
2023-03-25 00:50:49 +0530

    0.9.7

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -9,8 +9,12 @@ export default [
         replace: `$1,AccountDetails: () => accountDetails$2`,
       },
       {
-        match: /([^]*)var ([\w$]+)=(function[^]*?hoveringOnMute:!1};)/,
-        replace: `$1var $2,accountDetails;$2=accountDetails=$3`,
+        match:
+          /(?=[^]*var [\w$]+,accountDetails;[\w$]+=accountDetails=function[^]*?hoveringOnMute:!1};)|([^]*)var ([\w$]+)=(function[^]*?hoveringOnMute:!1};)/,
+        replace: (_: string, prefix: string, varName: string, suffix: string) => {
+          if (!prefix || !varName || !suffix) return `${_}`;
+          return `${prefix}var ${varName},accountDetails;${varName}=accountDetails=${suffix}`;
+        },
       },
     ],
   },
